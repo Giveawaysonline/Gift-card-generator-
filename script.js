5553a307ea302e7ff529a3b75b9645edfda8bb16
Giveawaysonline <155429474+Giveawaysonline@users.noreply.github.com>
2024-01-21 15:41:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-document.querySelectorAll('.card').forEach(function(card) {
-    card.addEventListener('click', function() {
-        var value = this.getAttribute('data-value');
-        alert('You have selected the $' + value + ' card. Complete any offer on the next page to get the gift card.');
-        // Redirect to CPA Grip offer URL
-        // window.location.href = 'https://singingfiles.com/1082027'; // Replace with your actual URL here
-    });
-});
+
